d8bf065cbdde1af4c2b7b6b22d50ca65fb4d4394
kcunitary <admin@kcunitary.pw>
2023-04-20 02:58:53 +0800

    fix v2 service

--- soEvil/services/proxy.scm
+++ soEvil/services/proxy.scm
@@ -8,86 +8,54 @@
   #:use-module (gnu services shepherd)
   #:use-module (gnu system shadow)
   #:use-module (soEvil packages proxy)
-  #:export (v2ray-configuration
-            v2ray-service-type))
-
+  #:export (v2ray-configuration v2ray-service-type))
 
 (define %default-v2ray-conf
-(plain-file "config.json" "{
-  \"log\": {
-    \"loglevel\": \"warning\"
-  },
-  \"inbounds\": [{
-    \"port\": 1080,
-    \"listen\": \"127.0.0.1\",
-    \"protocol\": \"socks\",
-    \"settings\": {
-      \"auth\": \"noauth\",
-      \"udp\": false,
-      \"ip\": \"127.0.0.1\"
-    }
-  }],
-  \"outbounds\": [{
-    \"protocol\": \"freedom\",
-    \"settings\": {},
-    \"tag\": \"direct\"
-  }],
-  \"policy\": {
-    \"levels\": {
-      \"0\": {\"uplinkOnly\": 0}
-    }
-  }
-}
-")
-)
+  (plain-file "config.json"
+              "{\n  \"log\": {\n    \"loglevel\": \"warning\"\n  },\n  \"inbounds\": [{\n    \"port\": 1080,\n    \"listen\": \"127.0.0.1\",\n    \"protocol\": \"socks\",\n    \"settings\": {\n      \"auth\": \"noauth\",\n      \"udp\": false,\n      \"ip\": \"127.0.0.1\"\n    }\n  }],\n  \"outbounds\": [{\n    \"protocol\": \"freedom\",\n    \"settings\": {},\n    \"tag\": \"direct\"\n  }],\n  \"policy\": {\n    \"levels\": {\n      \"0\": {\"uplinkOnly\": 0}\n    }\n  }\n}\n"))
 (define %v2ray-accounts
-  (list (user-group (name "v2ray") (system? #t))
+  (list (user-group
+          (name "v2ray")
+          (system? #t))
         (user-account
-         (name "v2ray")
-         (group "v2ray")
-         (system? #t)
-         (comment "v2ray user")
-         (home-directory "/var/empty")
-         (shell (file-append shadow "/sbin/nologin")))))
+          (name "v2ray")
+          (group "v2ray")
+          (system? #t)
+          (comment "v2ray user")
+          (home-directory "/var/empty")
+          (shell (file-append shadow "/sbin/nologin")))))
 
 (define-configuration/no-serialization v2ray-configuration
-  (v2ray
-   (package v2ray-bin)
-   "The v2ray package to use, we need @command{v2ray}.")
-  (config-file
-   (file-like %default-v2ray-conf)
-   "configuration files."))
+                                       (v2ray (package
+                                                v2ray-bin)
+                                        "The v2ray package to use, we need @command{v2ray}.")
+                                       (config-file (file-like
+                                                     %default-v2ray-conf)
+                                                    "configuration files."))
+
 (define v2ray-shepherd-service
   (match-lambda
     (($ <v2ray-configuration> v2ray-bin config-file)
-     (list (shepherd-service
-            (documentation "Run v2ray.")
-            (provision '(v2ray))
-            (requirement '(loopback))
-            (start #~(make-forkexec-constructor
-                      '(#$(file-append v2ray-bin "/bin/v2ray")
-                            "run" "-c" #$(file-append config-file))
-                      #:user "v2ray"
-                      #:group "v2ray"))
-            (stop #~(make-kill-destructor)))))))
-     (shepherd-service
-      (provision '(v2ray))
-      (requirement '(networking))
-      (documentation "")
-      (modules '((gnu build shepherd)
-                 (gnu system file-systems)))
-      (start #~(make-forkexec-constructor/container
-                #$v2ray-command
-                ;; #:namespaces '#$(delq 'net %namespaces)
-                #:log-file "/var/log/v2ray.log")
-      (stop #~(make-kill-destructor))))))
+     (list (shepherd-service (documentation "Run v2ray.")
+                             (provision '(v2ray))
+                             (requirement '(loopback))
+                             (start #~(make-forkexec-constructor '(#$(file-append
+                                                                      v2ray-bin
+                                                                      "/bin/v2ray")
+                                                                   "run" "-c"
+                                                                   #$config-file)
+                                                                 #:user
+                                                                 "v2ray"
+                                                                 #:group
+                                                                 "v2ray"))
+                             (stop #~(make-kill-destructor)))))))
 
 (define v2ray-service-type
-  (service-type
-   (name 'v2ray)
-   (extensions
-    (list (service-extension shepherd-root-service-type
-                             v2ray-shepherd-service)
-          ))
-   (default-value (v2ray-configuration))
-   (description "Run v2ray daemon.")))
+  (service-type (name 'v2ray)
+                (extensions (list (service-extension
+                                   shepherd-root-service-type
+                                   v2ray-shepherd-service)
+                                  (service-extension account-service-type
+                                                     (const %v2ray-accounts))))
+                (default-value (v2ray-configuration))
+                (description "Run v2ray daemon.")))
